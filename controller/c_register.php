e08ac54384a5a5fdf62d11d28afe0d77ea4fa987
dreuxfl <90611235+dreuxfl@users.noreply.github.com>
2021-11-09 14:39:51 +0100

    Mail is sent when trying to register, routes are secure

--- controller/c_register.php
+++ controller/c_register.php
@@ -24,46 +24,62 @@ switch ($_REQUEST['action']) {
             $objet = "";
 
             $_SESSION["CodeVerificationClt"] = mt_rand(10000, 99999);
-            $message = "Bonjour Mr." . $_SESSION["nom"] . ", <br> votre code  de confirmation est  :" . $_SESSION["CodeVerificationClt"] . ".<br> Cordialement, <br> -L'equipe Ryoken. <br> <center> <img src='https://pbs.twimg.com/profile_images/1348343673939963904/65yHDRre.png'></center>";
-            $key = "c86e0d7c45ca4b0ae07055e4687f4dc0";
+            $message = "". $_SESSION["nom"] . " ". $_SESSION["prenom"] . ", <p>Pour valider votre inscription, merci de saisir le code de confirmation suivant : " . $_SESSION["CodeVerificationClt"] . ".</p><p> Cordialement, <br>Le Présage. </p>";
+            //$key = "c86e0d7c45ca4b0ae07055e4687f4dc0";
             $mail = new PHPMailer();  // Cree un nouvel objet PHPMailer
-            //$mail->IsSMTP(); // active SMTP
+            
+            $mail->IsSMTP(); // active SMTP
             $mail->IsHTML(true);
-            $mail->SMTPDebug = 0;  // debogage: 1 = Erreurs et messages, 2 = messages seulement
-            $mail->SMTPAuth = true;  // Authentification SMTP active
-            $mail->SMTPSecure = 'ssl'; // Gmail REQUIERT Le transfert securise
+            $mail->CharSet = "UTF-8";
+
             $mail->Host = 'smtp.gmail.com';
+            $mail->Username = 'equipe8.hackaton@gmail.com';
+            //$mail->Password = md5($key);
+            $mail->Password = 'poubelux3000';
+
+            $mail->SMTPAuth = true;  // Authentification SMTP active
+            $mail->SMTPDebug = 0;  // debogage: 1 = Erreurs et messages, 2 = messages seulement
+            
+            $mail->SMTPSecure = 'ssl'; // Gmail REQUIERT Le transfert securise
+            
             $mail->Port = 465;
-            $mail->Username = 'ryoken.esport2021@gmail.com';
-            $mail->Password = md5($key);
-            $mail->SetFrom('ryoken.esport2021@gmail.com', 'Ryoken');
-            $mail->Subject = 'Confirmation inscription Ryoken';
+        
+            $mail->SetFrom('equipe8.hackaton@gmail.com', 'Le Présage');
+            $mail->Subject = 'Confirmation d\'inscription en tant qu\'investisseur';
             $mail->Body = $message;
-            $mail->AddAddress($_SESSION["mail"]);
+            $mail->AddAddress($_SESSION['mail']);
             if (!$mail->Send()) {
                 echo 'Mail error: ' . $mail->ErrorInfo;
             }
             include('./view/v_confirmationRegister.php');
         } else {
-            echo "<script type='text/javascript'> document.location.replace('index.php?action=150');</script>";
+            
+            header('location: index.php?request=30&action=50', true);
         }
         break;
 
     case VerificationCodeConfirmation:
-        if (isset($_POST["submitCltCodeVerfication"]) && $_POST["CodeVerificationClt"] == $_SESSION["CodeVerificationClt"]) {
-            $_SESSION["nom"] = $_POST["nom"];
-            $_SESSION["prenom"] = $_POST["prenom"];
-            $_SESSION["mail"] = $_POST["mail"];
-            $_SESSION["mdp"] = $_POST["mdp"];
-            echo addInvestor(md5($_SESSION["mdp"]), $_SESSION["prenom"], $_SESSION["nom"], $_SESSION["mail"]);
-            session_destroy();
-            include("./view/v_confirmationRegister.php");
-        } else if (isset($_POST["submitCltCodeVerfication"])  && $_POST["CodeVerificationClt"] !== $_SESSION["CodeVerificationClt"]) {
-            $er = "Le code de vérification est incorrect";
-            include("./view/v_confirmationRegister.php");
+        if (isset($_POST["confirmation"])) {
+            if (isset($_POST["confirmCode"])){
+                if($_POST["confirmCode"] == $_SESSION["CodeVerificationClt"]){
+                    $_SESSION["nom"] = $_POST["nom"];
+                    $_SESSION["prenom"] = $_POST["prenom"];
+                    $_SESSION["mail"] = $_POST["mail"];
+                    $_SESSION["mdp"] = $_POST["mdp"];
+                    echo addInvestor(md5($_SESSION["mdp"]), $_SESSION["prenom"], $_SESSION["nom"], $_SESSION["mail"]);
+                    session_destroy();
+                    include("./view/v_confirmationRegister.php");
+                }
+                // else{
+                //     $er = "Le code de vérification est incorrect";
+                //     include("./view/v_confirmationRegister.php");
+                // }  
+        
+            }
         } else {
-            echo "<script type='text/javascript'> document.location.replace('index.php?action=150');</script>";
-            exit();
+            // echo "<script type='text/javascript'> document.location.replace('index.php?action=150');</script>";
+            //  exit();
+            header('location: index.php?request=30&action=50', true);
         }
         break;
 }

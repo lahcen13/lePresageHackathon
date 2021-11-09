088d6c85aab5ed0ed6f57752313d6ef62e91c51d
fl0rian-andre <florian.andre@epitech.eu>
2021-11-09 18:11:19 +0100

    edit navbar

--- view/v_navbar.php
+++ view/v_navbar.php
@@ -2,15 +2,5 @@
     <a href="#">
         <p class="h3">Le Présage</p>
     </a>
-    <div class="topnav-right">
-        <a href="#">
-            <p class="h3 ">Login</p>
-        </a>
-        <a href="#">
-            <p class="h3 ">Register</p>
-        </a>
-        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-            <i class="fa fa-bars"></i>
-        </a>
-    </div>
+    
 </div>
